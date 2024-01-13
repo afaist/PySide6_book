4eca6e4ddd3ba6a8ee9ceecfc3e5731efbd859d8
afaist <afaist@gmail.com>
2024-01-13 11:31:44 +0700

    Two slots for one button

---
+++ lesson_2_2.py
@@ -0,0 +1,42 @@
+"""
+Lesson 2
+https://www.pythonguis.com/tutorials/pyside6-signals-slots-events/
+Signals, Slots & Events and get Data
+"""
+# Only needed for access to command line arguments
+import sys
+
+from PySide6.QtCore import QSize
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Мое приложение")
+
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
+
+        self.setMinimumSize(QSize(400, 300))
+        self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
+    def the_button_was_toggled(self, checked):
+        print("Checked?", checked)
+
+
+# You need one (and only one) QApplication isinstance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know yow won't use command line arguments QApplication([]) works too.
+app = QApplication(sys.argv)
+# Create a Qt widget, which will be our window.
+window = MainWindow()
+window.show()  # IMPORTANT!!! Windows are hidden by default.
+# Start the event loop.
+app.exec()
