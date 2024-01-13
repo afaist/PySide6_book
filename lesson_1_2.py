63ba6bdd7183719ece70ca3ba7c63843e4c61389
afaist <afaist@gmail.com>
2024-01-13 10:13:59 +0700

    First application with Main window

---
+++ lesson_1_2.py
@@ -0,0 +1,18 @@
+"""
+Lesson 1
+First application with MainWindow
+"""
+# Only needed for access to command line arguments
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+# You need one (and only one) QApplication isinstance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know yow won't use command line arguments QApplication([]) works too.
+app = QApplication(sys.argv)
+# Create a Qt widget, which will be our window.
+window = QMainWindow()
+window.show()  # IMPORTANT!!! Windows are hidden by default.
+# Start the event loop.
+app.exec()
