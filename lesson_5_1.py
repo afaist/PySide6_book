5e53942342d45ff3ac42a43606b5b918ed3024d4
afaist <afaist@gmail.com>
2024-01-15 17:51:47 +0700

    QToolBar QAction

---
+++ lesson_5_1.py
@@ -0,0 +1,38 @@
+"""
+https://www.pythonguis.com/tutorials/pyside6-actions-toolbars-menus/
+Toolbar, Statusbar and Menu
+QToolBar QAction
+"""
+import sys
+
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QToolBar
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle("Toolbar $ Menus")
+
+        label = QLabel("Hello!")
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.setCentralWidget(label)
+
+        toolbar = QToolBar("My main toolbar")
+        self.addToolBar(toolbar)
+
+        button_action = QAction("Your button", self)
+        button_action.setStatusTip("This is your button")
+        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        toolbar.addAction(button_action)
+
+    def onMyToolBarButtonClick(self, s):
+        print("Click", s)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
