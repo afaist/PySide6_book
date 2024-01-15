10f9f67cefe1c4de6cd88222a0010ea0dc514d5e
afaist <afaist@gmail.com>
2024-01-15 11:04:51 +0700

    QVBoxLayout

---
+++ lesson_4_1.py
@@ -0,0 +1,48 @@
+"""
+https://www.pythonguis.com/tutorials/pyside6-layouts/
+Макеты
+QVBoxLayout
+"""
+import sys
+
+from PySide6.QtGui import QColor, QPalette
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("Layouts QVBoxLayout")
+
+        layout = QVBoxLayout()
+
+        layout.addWidget(Color("red"))
+        layout.addWidget(Color("green"))
+        layout.addWidget(Color("blue"))
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+
+class Color(QWidget):
+    """
+    Вспомогательный класс для вывода цветных полос
+    """
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
