430bffbdae10d4d219df36673be43f17bb8f88d8
afaist <afaist@gmail.com>
2024-01-16 19:27:15 +0700

    Another window

---
+++ lesson_7_1.py
@@ -0,0 +1,45 @@
+"""
+https://www.pythonguis.com/tutorials/pyside6-creating-multiple-windows/
+Creating additional windows
+Another window
+"""
+
+import sys
+from random import randint
+
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton,
+                               QVBoxLayout, QWidget)
+
+
+class AnotherWindow(QWidget):
+    """
+    This window is a QWidget. If it has no parent, it will
+    appear as a free-floating window as we want.
+    """
+
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Another window %d" % randint(0, 100))
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.w = None  # No external window yet.
+        self.button = QPushButton("Push for Window")
+        self.button.clicked.connect(self.show_new_window)
+        self.setCentralWidget(self.button)
+
+    def show_new_window(self, checked):
+        if self.w is None:
+            self.w = AnotherWindow()
+        self.w.show()
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
