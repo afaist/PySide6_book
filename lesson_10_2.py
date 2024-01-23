53dec1581c76eac6a4728e1a7e1a1fcbf0cb1359
afaist <afaist@gmail.com>
2024-01-23 11:37:01 +0700

    BAD solution for threads

---
+++ lesson_10_2.py
@@ -0,0 +1,53 @@
+"""
+https://www.pythonguis.com/tutorials/multithreading-pyside6-applications-qthreadpool/
+Multithreading application with QThreadPool
+add QApplication.processEvents()
+"""
+import sys
+import time
+
+from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton,
+                               QVBoxLayout, QWidget)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.counter = 0
+
+        layout = QVBoxLayout()
+
+        self.label = QLabel("Start")
+        button = QPushButton("DANGER!")
+        button.pressed.connect(self.oh_no)
+
+        layout.addWidget(self.label)
+        layout.addWidget(button)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
+
+        self.show()
+
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.recurring_timer)
+        self.timer.start()
+
+    def oh_no(self):
+        for _ in range(5):
+            QApplication.processEvents()
+            time.sleep(5)
+
+    def recurring_timer(self):
+        self.counter += 1
+        self.label.setText("Counter: %d" % self.counter)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+app.exec()
