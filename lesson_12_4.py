096ac29d94d91fa96157d1da8758753cc7b12db3
afaist <afaist@gmail.com>
2024-01-26 18:18:40 +0700

    Draw rectangles

---
+++ lesson_12_4.py
@@ -0,0 +1,54 @@
+"""
+https://www.pythonguis.com/tutorials/pyside6-bitmap-graphics/
+QPainter and Bitmap Graphics
+DrawLine, drawRect, drawRects and drawRoundedRect
+"""
+import sys
+from random import choice, randint
+
+from PySide6 import QtGui
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Draw rects")
+        self.resize(400, 400)
+
+        self.label = QLabel()
+        canvas = QtGui.QPixmap(400, 300)
+        canvas.fill(Qt.GlobalColor.white)
+        self.label.setPixmap(canvas)
+        self.setCentralWidget(self.label)
+        self.draw_something()
+
+    def draw_something(self):
+        colors = ["#FFD141", "#376F9F", "#0D1F2D", "#19FBBB", "#EB5160"]
+        canvas = self.label.pixmap()
+        painter = QtGui.QPainter(canvas)
+        pen = QtGui.QPen()
+        pen.setWidth(5)
+        painter.setPen(pen)
+        brush = QtGui.QBrush()
+        brush.setStyle(Qt.BrushStyle.Dense1Pattern)
+        for n in range(10):
+            pen.setColor(QtGui.QColor(choice(colors)))
+            painter.setPen(pen)
+            brush.setColor(QtGui.QColor(choice(colors)))
+            painter.setBrush(brush)
+            painter.drawRect(randint(0, 100), randint(0, 100), randint(100, 250), randint(100, 250))
+
+        pen.setWidth(40)
+        pen.setColor(QtGui.QColor("red"))
+        painter.setPen(pen)
+        painter.drawPoint(300, 150)
+        painter.end()
+        self.label.setPixmap(canvas)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
